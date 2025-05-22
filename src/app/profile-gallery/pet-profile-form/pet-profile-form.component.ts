e49213d3e1f1a2703422599ac909fc2da111edab
nicolas bluj <nicolas_bluj@acslworld.com>
2025-05-22 15:21:28 +0200

    codelab 05 --> made a seperate component for adding a pet with some comments on @input and @output with eventemitter

--- src/app/profile-gallery/pet-profile-form/pet-profile-form.component.ts
+++ src/app/profile-gallery/pet-profile-form/pet-profile-form.component.ts
@@ -1,11 +1,51 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, inject, Input, Output} from '@angular/core';
+import {PetService} from '../../service/pet.service';
+import {FormBuilder, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import {Pet} from '../../model/Pet';
+import {PetForm} from '../../model/PetForm';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-pet-profile-form',
-  imports: [],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './pet-profile-form.component.html',
   styleUrl: './pet-profile-form.component.css'
 })
 export class PetProfileFormComponent {
+  readonly petService= inject(PetService);
+  readonly #formBuilder = inject(FormBuilder)
+  @Output() petsChanged = new EventEmitter<Pet[]>();
+
+  petForm = this.#formBuilder.nonNullable.group<PetForm>({
+    id: this.#formBuilder.nonNullable.control('', Validators.required),
+    name: this.#formBuilder.nonNullable.control('', Validators.required),
+    kind: this.#formBuilder.nonNullable.control('', Validators.required),
+    image: this.#formBuilder.nonNullable.control('', Validators.required),
+    profileText: this.#formBuilder.nonNullable.control('', Validators.required),
+    popularity: this.#formBuilder.nonNullable.control('', Validators.required)
+  })
+
+  onSubmit() {
+    console.warn(this.petForm.value);
+    const petForm = this.petForm.value
+    const pet: Pet = {
+      id: Number(petForm.id),
+      name: petForm.name || '',
+      kind: petForm.kind || '',
+      image: petForm.image || '',
+      profileText: petForm.profileText || '',
+      popularity: Number(petForm.popularity)
+    }
+    this.petService.addPet(pet).subscribe({ next: () => {
+        this.petsChanged.emit() //next: is manditory, only after subscription do we emit the change
+      }
+    })
+  }
+
+  constructor() {
+  }
 
 }
--- src/app/profile-gallery/profile-gallery.component.ts
+++ src/app/profile-gallery/profile-gallery.component.ts
@@ -1,58 +1,25 @@
-import {Component, EventEmitter, inject, Input, Output} from '@angular/core';
+import {Component, inject, Input} from '@angular/core';
 import {PetService} from '../service/pet.service';
 import {Pet} from '../model/Pet';
 import {AsyncPipe} from '@angular/common';
-import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
 import {NameFilterPipe} from '../pipes/name-filter.pipe';
-import {PetForm} from '../model/PetForm';
-import {Observable} from 'rxjs';
+import {PetProfileFormComponent} from './pet-profile-form/pet-profile-form.component';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-profile-gallery',
-  imports: [AsyncPipe, FormsModule, NameFilterPipe, ReactiveFormsModule],
+  imports: [AsyncPipe, NameFilterPipe, PetProfileFormComponent, FormsModule],
   templateUrl: './profile-gallery.component.html',
   styleUrl: './profile-gallery.component.css'
 })
 export class ProfileGalleryComponent{
   readonly petService= inject(PetService);
-  readonly #formBuilder = inject(FormBuilder)
   @Input() pets$ = this.petService.getPets();
-  @Output() petsChanged = new EventEmitter<Pet[]>();
   selectedPet: Pet
   searchText: string = ''
 
-  petForm = this.#formBuilder.nonNullable.group<PetForm>({
-    id: this.#formBuilder.nonNullable.control('', Validators.required),
-    name: this.#formBuilder.nonNullable.control('', Validators.required),
-    kind: this.#formBuilder.nonNullable.control('', Validators.required),
-    image: this.#formBuilder.nonNullable.control('', Validators.required),
-    profileText: this.#formBuilder.nonNullable.control('', Validators.required),
-    popularity: this.#formBuilder.nonNullable.control('', Validators.required)
-  })
-
-  onSubmit() {
-    console.warn(this.petForm.value);
-    const petForm = this.petForm.value
-    const pet: Pet = {
-      id: Number(petForm.id),
-      name: petForm.name || '',
-      kind: petForm.kind || '',
-      image: petForm.image || '',
-      profileText: petForm.profileText || '',
-      popularity: Number(petForm.popularity)
-    }
-    this.petService.addPet(pet).subscribe({ next: () => {
-      this.refreshPets(this.petService.getPets())
-    }
-    })
-  }
-
-  refreshPets(newPets: Observable<Pet>): void {
-    this.pets$ = newPets
-    this.petsChanged.emit()
-  }
-
-  constructor() {
+  refreshPets(): void {
+    this.pets$ = this.petService.getPets()
   }
 
   isPetSelected() {
@@ -62,6 +29,9 @@ export class ProfileGalleryComponent{
   selectPet(pet: Pet) {
     this.selectedPet = pet
     console.log(this.selectedPet);
+  }
+
+  constructor() {
   }
 
 }
