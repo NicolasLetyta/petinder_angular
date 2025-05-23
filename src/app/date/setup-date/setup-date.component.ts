f166d4be9f1669c02693441ea3f443f5d9fecbd9
nicolas bluj <nicolas_bluj@acslworld.com>
2025-05-23 11:19:50 +0200

    codelab 06 --> noticed a cancel button in the setup-date.component.html so added routerLink to to the main page

--- src/app/date/setup-date/setup-date.component.ts
+++ src/app/date/setup-date/setup-date.component.ts
@@ -1,35 +1,50 @@
 import {Component, inject, OnInit} from '@angular/core';
 import {PetService} from '../../service/pet.service';
-import {ActivatedRoute} from '@angular/router';
-import {map, Observable} from 'rxjs';
-import { Routes } from '@angular/router';
+import {ActivatedRoute, RouterLink, RouterLinkActive} from '@angular/router';
+import {Observable} from 'rxjs';
 import {Pet} from '../../model/Pet';
 import {AsyncPipe} from '@angular/common';
-import {FormGroup ,ReactiveFormsModule} from '@angular/forms';
+import {FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-setup-date',
   imports: [
     AsyncPipe,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterLinkActive,
+    RouterLink,
   ],
   templateUrl: './setup-date.component.html',
   styleUrl: './setup-date.component.css'
 })
 export class SetupDateComponent implements OnInit{
   private readonly route = inject(ActivatedRoute)
+  readonly #formBuilder = inject(FormBuilder)
   readonly #petService = inject(PetService)
-  sendTextForm: FormGroup //temp: so that template renders
+
   selectedPetName: string|null
   selectedPet: Observable<Pet>
-  //selectedPetName = this.route.snapshot.paramMap.get('name')
-  //selectedPet = this.#petService.getPetByName(this.selectedPetName || '')
 
+  dateForm = this.#formBuilder.nonNullable.group({
+    name: this.#formBuilder.nonNullable.control('', Validators.required),
+  }) //Not really sure what the form is supposed to be so for now its the name of the person that wants
+  // to arrange a date!
+
+  // SUPER IMPORTANT!!!! -> JUST BECAUSE IM USING THE NEW WAY OF INJECTING WITH "inject()",
+  // DOES NOT MEAN THAT THE OnInit template AND ngOnInit() ARE USELESS, YES I CAN INITIALIZE THE
+  // ROUTE PARAM WITH SNAPSHOT, BUT ngOnInit() ALLOWS ME TO LOG AT INITIALIZATION WHICH LITERALLY
+  // GIVES ME ALL THE INFO I NEED TO DEBUG THE GET REQUEST!!!
   ngOnInit() {
     this.selectedPetName = this.route.snapshot.paramMap.get('name')
     this.selectedPet = this.#petService.getPetByName(this.selectedPetName || '')
     console.log(this.selectedPetName)
-    console.log(this.selectedPet)
+  }
+
+  //can this method be called anything? What if I have 2 forms on the page? -> i would need 2 onSubmit methods
+  //they would then be bound to 2 buttons meaning i would need to separate logic by having different method names?
+  onSubmit() {
+    // logic for setting up a date!!
+    console.log("Button was pressed, logic for date inc>")
   }
 
 }
